22799f382cc167f02f70f4ef89c87115080f95df
trippsupp <trippsupp@gmail.com>
2017-01-20 23:18:28 -0500

    Inital commit

---
+++ MetricConverter/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MetricConverter
+{
+    partial class frame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.lblConversion = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Meters to feet/inches",
+            "Feet/inches to meters"});
+            this.comboBox.Location = new System.Drawing.Point(101, 6);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(136, 21);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(101, 61);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(45, 20);
+            this.txtInput1.TabIndex = 1;
+            // 
+            // lblConversion
+            // 
+            this.lblConversion.AutoSize = true;
+            this.lblConversion.Location = new System.Drawing.Point(12, 9);
+            this.lblConversion.Name = "lblConversion";
+            this.lblConversion.Size = new System.Drawing.Size(83, 13);
+            this.lblConversion.TabIndex = 2;
+            this.lblConversion.Text = "Conversion type";
+            this.lblConversion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(25, 64);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(0, 13);
+            this.lblInput.TabIndex = 3;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(25, 93);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(101, 90);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(222, 73);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(156, 61);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(45, 20);
+            this.txtInput2.TabIndex = 7;
+            this.txtInput2.Visible = false;
+            // 
+            // frame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 137);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblConversion);
+            this.Controls.Add(this.txtInput1);
+            this.Controls.Add(this.comboBox);
+            this.Name = "frame";
+            this.Text = "Vance Field | Metric Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.Label lblConversion;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtInput2;
+    }
+}
+
---
+++ MetricConverter/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetricConverter
+{
+    public partial class frame : Form
+    {
+
+        // global variables
+        private double meters;
+        private double inches;
+        private int feet;
+
+        public frame()
+        {
+            InitializeComponent();
+            comboBox.SelectedIndex = 0; // default selection for combo box
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBox.SelectedIndex; // get conversion selection 
+
+            // if meters to feet/in
+            if(index == 0)
+            {
+                lblInput.Text = "Meters";
+                lblOutput.Text = "Feet/Inches";
+                txtInput2.Visible = false;
+            }
+
+            // if feet/in to meters
+            if(index == 1)
+            {
+                lblInput.Text = "Feet / Inches";
+                lblOutput.Text = "Meters";
+                txtInput2.Visible = true;
+            }
+
+            // clear text fields
+            txtInput1.Text = "";
+            txtInput2.Text = "";
+            txtOutput.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {            
+            int index = comboBox.SelectedIndex; // take input
+
+            // if meters to feet/inches
+            if (index == 0)
+            {
+                try { 
+                    meters = Double.Parse(txtInput1.Text);
+                    inches = meters * 39.37;
+                    feet = (int) inches / 12;
+                    double remInches = inches - (feet * 12); // remaining inches
+                    txtOutput.Text = feet.ToString() + " ft " + remInches.ToString("0.##") + " in";
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine(er.Source);
+                }
+            }
+
+            // if feet/inches to meters
+            if (index == 1)
+            {
+                try { 
+                    feet = Int32.Parse(txtInput1.Text);
+                    double ftInInches = feet * 12;
+                    inches = Double.Parse(txtInput2.Text) + ftInInches;
+                    meters = inches / 39.37;              
+                    txtOutput.Text = meters.ToString("0.##");
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine(er.Source);
+                }
+            }
+        }
+    }
+}
